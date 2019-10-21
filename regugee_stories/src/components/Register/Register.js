90403fb7b356806587d608f3045b7be918bbcc0e
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-10-21 11:44:37 -0700

    register working

--- regugee_stories/src/components/Register/Register.js
+++ regugee_stories/src/components/Register/Register.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import './Register.css'
+import React, { useState } from 'react';
+import './Register.css';
+import axios from 'axios';
 
-function Register() {
+function Register(props) {
+
+    const [credentials, setCreadentials] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+    })
+
+    const handleChange = e => {
+        setCreadentials({...credentials, [e.target.name]: e.target.value})
+        console.log('input value', credentials)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+        .post("https://refugee-stories-api19.herokuapp.com/auth/register", credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.token)
+            props.history.push('/')
+        })
+        .catch(err => console.log(err))
+    };
+
+    
     return (
         <div className='Register'>   
             <h1>Register</h1>
-            <form className='register-form'>
-                <input placeholder='Email'></input>
-                <input placeholder='Username'></input>
-                <input placeholder='Password'></input>
+            <form onSubmit={handleSubmit} className='register-form'>
+                <input
+                onChange={handleChange} 
+                type='email' 
+                name='email' 
+                placeholder='Email'>
+                </input>
+                <input
+                onChange={handleChange}
+                type='text' 
+                name='firstName' 
+                placeholder='First Name'>
+                </input>
+                <input
+                onChange={handleChange}
+                type='text' 
+                name='lastName' 
+                placeholder='Last Name'>
+                </input>
+                <input
+                onChange={handleChange} 
+                type='password'
+                name='password' 
+                placeholder='Password'>
+                </input>
                 <button>Submit</button>
             </form> {/* register-form end */}
         </div> /* Register end */
